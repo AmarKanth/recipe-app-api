788fe054ff78fb54476a4e42de8cbaa282cbc418
AmarKanth <amarkanth.bollu@gmail.com>
2020-11-14 12:14:32 +0530

    added tags app

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
+    path('ap/recipe/', include('recipe.urls')),
 ]
